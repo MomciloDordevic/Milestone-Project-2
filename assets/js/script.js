e15f5e283a5aa1aae04b72cd0c5aed9c1a242f4a
MomciloDordevic <qwertyumom@gmail.com>
2021-04-20 19:19:08 +0000

    added var ImgSource array

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,13 @@
 var Source = "#cards";
 var ImgSource = [
-    "assets/img/flags/afganistan",
-    
-]
+    "assets/img/flags/Afganistan.gif",
+    "assets/img/flags/Argentina.gif",
+    "assets/img/flags/Bosnia.gif",
+    "assets/img/flags/Chile.gif",
+    "assets/img/flags/Congo.gif",
+    "assets/img/flags/Finland.gif",
+    "assets/img/flags/France.gif",
+    "assets/img/flags/Germany.gif",
+    "assets/img/flags/Iceland.gif",
+    "assets/img/flags/Ireland.gif"
+];
