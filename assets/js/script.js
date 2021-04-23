9fac39a179c949725222fd270c54c69de1172df8
MomciloDordevic <qwertyumom@gmail.com>
2021-04-23 16:08:19 +0000

    added cards to js and added an event listener for a click, writing flipcard function next

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,13 +1,3 @@
-var Source = "#cards";
-var ImgSource = [
-    "assets/img/flags/Afganistan.gif",
-    "assets/img/flags/Argentina.gif",
-    "assets/img/flags/Bosnia.gif",
-    "assets/img/flags/Chile.gif",
-    "assets/img/flags/Congo.gif",
-    "assets/img/flags/Finland.gif",
-    "assets/img/flags/France.gif",
-    "assets/img/flags/Germany.gif",
-    "assets/img/flags/Iceland.gif",
-    "assets/img/flags/Ireland.gif"
-];
+const cards = document.querySelectorAll(".game-card");
+
+cards.forEach(card =>card.addEventListener("click", "flipcard"));
